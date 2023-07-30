d39dbec9b9ef4334867abd9f8009f3c250d4a572
iamchapita <alejandrom646@gmail.com>
2023-07-30 13:19:07 -0600

    Implemtanción de pantalla de login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+	faFacebook = faFacebook;
 }
