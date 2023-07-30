c96b5c541dc7cc0141b01a938c98f216b4053209
iamchapita <alejandrom646@gmail.com>
2023-07-30 16:04:08 -0600

    Implementacionde navbar

--- src/app/codeverse/codeverse.module.ts
+++ src/app/codeverse/codeverse.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeverseRoutingModule } from './codeverse-routing.module';
 import { ProjectExplorerComponent } from './pages/project-explorer/project-explorer.component';
+import { SharedModule } from '../shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-	// declarations: [HomeComponent, CreatorComponent, FilesComponent],
-	imports: [CommonModule, CodeverseRoutingModule],
-	declarations: [
-   ProjectExplorerComponent
+	declarations: [ProjectExplorerComponent],
+	imports: [
+		CommonModule,
+		CodeverseRoutingModule,
+		SharedModule,
+		FontAwesomeModule,
 	],
 })
 export class CodeverseModule {}
