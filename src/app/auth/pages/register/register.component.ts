23bb01a8a97b9eb343eb4b3db484e4700cdcaf62
iamchapita <alejandrom646@gmail.com>
2023-07-30 14:50:48 -0600

    Implemetnacion de fontawesome

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
+	faFacebook = faFacebook;
 }
