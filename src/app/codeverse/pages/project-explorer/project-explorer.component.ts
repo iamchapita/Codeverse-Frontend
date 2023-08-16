5af5709d21b0c5ed94860b3bc471f570ca5ffba3
iamchapita <alejandrom646@gmail.com>
2023-08-16 01:34:05 -0600

    Implementación de servicio

--- src/app/codeverse/pages/project-explorer/project-explorer.component.ts
+++ src/app/codeverse/pages/project-explorer/project-explorer.component.ts
@@ -10,6 +10,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 
 import { Folder } from 'src/models/Folder.model';
+import { FetchService } from 'src/app/services/fetch.service';
 
 @Component({
 	selector: 'app-project-explorer',
@@ -27,56 +28,7 @@ export class ProjectExplorerComponent {
 
 	isExplorer: boolean = true;
 
-	data: Array<Folder> = [
-		{
-			_id: '1',
-			name: 'Raíz',
-			description: '',
-			folders: [
-				{
-					_id: '1',
-					name: 'Carpeta 1 en 1',
-					description: 'Prueba de Carpeta',
-					createdAt: '2023-07-20',
-					modifiedAt: '2023-07-30',
-					folders: [],
-					projects: [],
-				},
-			],
-			projects: [
-				{
-					_id: '1',
-					name: 'Proyecto 1',
-					description: 'Proyecto 1 en  Web',
-					createdAt: '2023-07-20',
-					modifiedAt: '2023-07-30',
-					files: [
-						{
-							_id: '1',
-							type: 'HTML',
-							content: '',
-							createdAt: '2023-07-20',
-							modifiedAt: '2023-07-30',
-						},
-						{
-							_id: '2',
-							type: 'CSS',
-							content: '',
-							createdAt: '2023-07-20',
-							modifiedAt: '2023-07-30',
-						},
-						{
-							_id: '3',
-							type: 'JS',
-							content: '',
-							createdAt: '2023-07-20',
-							modifiedAt: '2023-07-30',
-						},
-					],
-				},
-			],
-			createdAt: '2023-07-20',
-			modifiedAt: '2023-07-30',
-		},
-	];
+	constructor(private fetchService: FetchService) {}
+
+	rootFolder: Folder = this.fetchService.rootFolder;
 }
