2b14a8b474ec9a66aacc856964b140530b03a944
iamchapita <alejandrom646@gmail.com>
2023-08-17 13:04:06 -0600

    Configuración de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { firebaseConfig } from '../environments/firebaseConfig';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -8,7 +13,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, NgbModule],
+	imports: [
+		AngularFireModule.initializeApp(firebaseConfig),
+		AngularFireAuthModule,
+		BrowserModule,
+		AppRoutingModule,
+		FontAwesomeModule,
+		NgbModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyDFUOPYtcxJWO-1NacnvrPGPeBNvRwOn9M",
+	authDomain: "codeverse-5d8c1.firebaseapp.com",
+	projectId: "codeverse-5d8c1",
+	storageBucket: "codeverse-5d8c1.appspot.com",
+	messagingSenderId: "131283937714",
+	appId: "1:131283937714:web:5a478fbc89bd82d1a21cab"
+  };
