e54c382e5f9b6c8bb110aa2c97de5e2e3172d990
iamchapita <alejandrom646@gmail.com>
2023-08-24 15:10:53 -0600

    Implementación de componente para agregar carpeta, proyecto o snippet

---
+++ src/app/codeverse/components/create-new-action/create-new-action.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-action',
+  templateUrl: './create-new-action.component.html',
+  styleUrls: ['./create-new-action.component.css']
+})
+export class CreateNewActionComponent {
+
+}
